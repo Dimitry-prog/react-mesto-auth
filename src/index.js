c715445f2da5975ccab1d129944f37dafba508ac
Dimitry-prog <kfifa3@gmail.com>
2022-12-26 23:49:13 +0300

    fix: base homepage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import {AppProvider} from "./context/AppContext";
-import {BrowserRouter} from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
+import { BrowserRouter } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/react-mesto-auth/`}>
+    <BrowserRouter>
       <AppProvider>
-        <App/>
+        <App />
       </AppProvider>
     </BrowserRouter>
   </React.StrictMode>
